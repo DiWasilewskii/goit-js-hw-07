d516cecce7e37a0a7fe6650b80c7b6c7c2f5a410
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-06-17 17:36:08 +0300

    end

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,13 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+const galleryList = document.querySelector('.gallery');
+
+const markupImages = images
+  .map(
+    image =>
+      `<li class='gallery-item'><img src="${image.url}" alt="${image.alt}" /></li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', markupImages);
